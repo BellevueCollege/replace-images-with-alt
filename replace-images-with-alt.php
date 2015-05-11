e1eb9a21c13b1c7881d12dd4c7844a68efe99fe5
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-05-11 15:21:07 -0700

    Correct Version # to 0.0.0.1

--- replace-images-with-alt.php
+++ replace-images-with-alt.php
@@ -3,7 +3,7 @@
  * Plugin Name: Replace All Images with Alt Text
  * Plugin URI: https://github.com/BellevueCollege/replace-images-with-alt/
  * Description: Enqueues jQuery to replace all images with their alt text. Designed to raise awareness for Global Accessibility Awareness Day 2015.
- * 0.0.1
+ * 0.0.0.1
  * Author: Bellevue College Information Technology Services
  * Author URI: http://www.bellevuecollege.edu
  * License:
