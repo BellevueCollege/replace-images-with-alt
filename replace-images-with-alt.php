503b880cdbf930fa81103ea6a895b0775672b4de
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-07-20 12:59:21 -0700

    Add Licence

--- replace-images-with-alt.php
+++ replace-images-with-alt.php
@@ -3,10 +3,24 @@
  * Plugin Name: Replace All Images with Alt Text
  * Plugin URI: https://github.com/BellevueCollege/replace-images-with-alt/
  * Description: Enqueues jQuery to replace all images with their alt text. Designed to raise awareness for Global Accessibility Awareness Day 2015.
- * 1.0
+ * 1.0.0.1
  * Author: Bellevue College Information Technology Services
  * Author URI: http://www.bellevuecollege.edu
- */
+ * License: GPL2
+
+{Plugin Name} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+{Plugin Name} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
+*/
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
